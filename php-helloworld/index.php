15c957ac656d7a7960a6ac278935a124b24c879e
Student User <student@workstation.lab.example.com>
2020-05-19 19:41:30 +0000

    changed index page contents

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,4 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "A change is coming from SUYASH KARMARKAR "\n";
+print "Hello, World! change is coming php version is " . PHP_VERSION . "\n";
 ?>
